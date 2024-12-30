ce5b2b351b3f2404b4afd4765fedaae3183e4ffc
davyalex <alexkouamelan96pv@gmail.com>
2024-12-30 16:20:49 +0000

    first deploy on dev

--- resources/views/admin/pages/dashboard_home.blade.php
+++ resources/views/admin/pages/dashboard_home.blade.php
@@ -12,9 +12,8 @@
                     <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                             <div class="card-content">
-                                <h5 class="font-15">New Booking</h5>
+                                <h5 class="font-15">Matieres</h5>
                                 <h2 class="mb-3 font-18">258</h2>
-                                <p class="mb-0"><span class="col-green">10%</span> Increase</p>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -34,9 +33,9 @@
                     <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                             <div class="card-content">
-                                <h5 class="font-15"> Customers</h5>
-                                <h2 class="mb-3 font-18">1,287</h2>
-                                <p class="mb-0"><span class="col-orange">09%</span> Decrease</p>
+                                <h5 class="font-15"> Niveaux</h5>
+                                <h2 class="mb-3 font-18">20</h2>
+                               
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -56,10 +55,9 @@
                     <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                             <div class="card-content">
-                                <h5 class="font-15">New Project</h5>
+                                <h5 class="font-15">Sujets</h5>
                                 <h2 class="mb-3 font-18">128</h2>
-                                <p class="mb-0"><span class="col-green">18%</span>
-                                    Increase</p>
+                               
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -79,9 +77,8 @@
                     <div class="row ">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
                             <div class="card-content">
-                                <h5 class="font-15">Revenue</h5>
-                                <h2 class="mb-3 font-18">$48,697</h2>
-                                <p class="mb-0"><span class="col-green">42%</span> Increase</p>
+                                <h5 class="font-15">Telechargements</h5>
+                                <h2 class="mb-3 font-18">458</h2>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
